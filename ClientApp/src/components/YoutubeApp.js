651451a49e458f611ce2240b5668a746dee7fa05
thomasv7877 <tvandenhauwe@gmail.com>
2023-05-22 20:32:20 +0200

    clean up search page

--- ClientApp/src/components/YoutubeApp.js
+++ ClientApp/src/components/YoutubeApp.js
@@ -1,26 +1,30 @@
 import React, { useState } from "react";
 //import { Download, Envelope } from "bootstrap-icons/icons";
+import '../custom.css'
 
 export function YoutubeApp(){
     const [searchTerm, setSearchTerm] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     //const apiKey = 'AIzaSyDnMozDltngJNf45Dnel-Xxo1Gm-Q0_uUU';
     const apiKey = process.env.react_app_yt_api_key;
-    const apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${searchTerm}&type=video&key=${apiKey}`;
+    const apiUrl = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchTerm}&type=video&key=${apiKey}`;
     const [searchResults, setSearchResults] = useState([]);
-    const testVar = "https://www.youtube.com/watch?v=1cBZcpSeiFc&pp=ygUTcGF5YmFjayBqYW1lcyBicm93bg%3D%3D";
+    //const testVar = "https://www.youtube.com/watch?v=1cBZcpSeiFc&pp=ygUTcGF5YmFjayBqYW1lcyBicm93bg%3D%3D";
 
     const handleSearch = async () => {
         try {
+          setIsLoading(true);
           const response = await fetch(apiUrl);
           const data = await response.json();
           console.log(data);
           setSearchResults(data.items);
+          setIsLoading(false);
         } catch (error) {
           console.error('Error:', error);
         }
       };
 
-    const handlePostTest = async (vidUrl) => {
+    const handleDownload = async (vidUrl) => {
       setSearchResults([]);
       try {
         const response = await fetch("/ytApi", {
@@ -35,24 +39,32 @@ export function YoutubeApp(){
         //console.error('Error:', error);
       }
     }
+    const handleKeyDown = (event) => {
+      if(event.key === 'Enter'){
+        handleSearch();
+      }
+    }
 
     return(
         <>
         <p>Find something on youtube</p>
-        <input type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}/>
-        <button type="button" className="btn btn-primary" onClick={handleSearch}>Search</button>
-        <button type="button" className="btn btn-primary" onClick={() => handlePostTest(testVar)}>Post test & Clear results</button>
-        <ul>
+        <input id="searchYt" type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} onKeyDown={handleKeyDown} placeholder="Search songs..."/>
+        {/*<button type="button" className="btn btn-primary btn-sm" onClick={handleSearch}>Search</button>
+        <button type="button" className="btn btn-primary" onClick={() => handlePostTest(testVar)}>Post test & Clear results</button>*/}
+        {!isLoading ? (
+        <ul id="ytResults">
         {searchResults.map(result => 
         {
             var vidLink = `https://www.youtube.com/watch?v=${result.id.videoId}`;
         return (
           <li key={result.id.videoId}>
-            <img src={result.snippet.thumbnails.default.url}></img>
-            <p>{result.snippet.title} <a href={vidLink}>Watch!</a> or <button type="button" className="btn" onClick={() => handlePostTest(vidLink)}><i className="icon bi-download"></i></button></p>
+            <a href={vidLink}><img src={result.snippet.thumbnails.default.url} alt={result.snippet.title}></img></a>
+            <p>{result.snippet.title} <a href={vidLink}>Watch!</a> or <button type="button" className="btn" onClick={() => handleDownload(vidLink)}><i className="icon bi-download"></i></button></p>
             </li>
         )})}
-      </ul>
+      </ul>) : (<div className="spinner-grow text-primary m-5" role="status">
+  <span className="sr-only">test</span>
+</div>)}
         </>
     );
 }
