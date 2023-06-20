3eb2ad4574bbf7be46b7571986e3d9c410d00079
thomasv7877 <tvandenhauwe@gmail.com>
2023-06-20 15:00:24 +0200

    finish front code cleanup and doc

--- ClientApp/src/components/AudioVisualizer.js
+++ ClientApp/src/components/AudioVisualizer.js
@@ -8,32 +8,11 @@ export default function AudioVizualizer({audioState}){
     const analyzerRef = useRef(null);
     const sourceRef = useRef(null);
 
-
-    /*const audioAnalyzer = () => {
-        // create a new AudioContext
-        audioCtxRef.current = new (window.AudioContext || window.webkitAudioContext)();
-        const audioCtx = audioCtxRef.current;
-        // create an analyzer node with a buffer size of 2048
-        analyzerRef.current = audioCtx.createAnalyser();
-        const analyzer = analyzerRef.current;
-        analyzer.fftSize = 256; // original was 2048
+    /*
+    visualizer: audio context with analyzer on the audio source, each time new audio data is available the canvas gets updated
+    */
     
-        const bufferLength = analyzer.frequencyBinCount;
-        const dataArray = new Uint8Array(bufferLength);
-        sourceRef.current = audioCtx.createMediaElementSource(audioState);
-        var source = sourceRef.current;
-        console.log("Created a source");
-        source.connect(analyzer);
-        source.connect(audioCtx.destination);
-        source.onended = () => {
-            source.disconnect();
-            console.log("Disconnected a source");
-        };
-    
-        // set the analyzerData state with the analyzer, bufferLength, and dataArray
-        setAnalyzerData({ analyzer, bufferLength, dataArray });
-      };*/
-    
+      // draw bars in the canvas based on analyzerData
       function animateBars(analyser, canvas, canvasCtx, dataArray, bufferLength) {
         // Analyze the audio data using the Web Audio API's `getByteFrequencyData` method.
         analyser.getByteFrequencyData(dataArray);
@@ -41,12 +20,8 @@ export default function AudioVizualizer({audioState}){
         // Set the canvas fill style to black.
         canvasCtx.fillStyle = '#000';
       
-        // Calculate the height of the canvas.
-        //const HEIGHT = canvas.height / 2;
-        const HEIGHT = canvas.height;
-      
         // Calculate the width of each bar in the waveform based on the canvas width and the buffer length.
-        var barWidth = Math.ceil(canvas.width / bufferLength); // original was * 2.5
+        var barWidth = Math.ceil(canvas.width / bufferLength); // original was * 2.5, edit depending on fftsize
       
         // Initialize variables for the bar height and x-position.
         let barHeight;
@@ -55,21 +30,13 @@ export default function AudioVizualizer({audioState}){
         // Loop through each element in the `dataArray`.
         for (var i = 0; i < bufferLength; i++) {
           // Calculate the height of the current bar based on the audio data and the canvas height.
-          barHeight = (dataArray[i] / 255) * HEIGHT;
+          barHeight = (dataArray[i] / 255) * canvas.height;
       
-          // Generate random RGB values for each bar.
-          //const maximum = 10;
-          //const minimum = -10;
-          //var r = 242 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-          //var g = 104 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-          //var b = 65 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+          // blue bar color, same as audio playback element
           var r = 0; var g = 169; var b = 223;
-      
-          // Set the canvas fill style to the random RGB values.
           canvasCtx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
       
           // Draw the bar on the canvas at the current x-position and with the calculated height and width.
-          //canvasCtx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
           canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
       
           // Update the x-position for the next bar.
@@ -77,17 +44,16 @@ export default function AudioVizualizer({audioState}){
         }
       }
     
-      // Function to draw the waveform
+      // Function to draw the waveform each time the analyzerData changes, animation frame to smooth transition, canvas cleared each update
       const drawCallback = useCallback(() => {
           const canvas = canvasRef.current;
           if (!canvas || !analyzerData) {
-            //nothing
+            // do nothing if no canvas or data
           } else {
             const canvasCtx = canvas.getContext("2d");
         
             const animate = () => {
                 requestAnimationFrame(animate);
-                //canvas.width = canvas.width;
                 canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
                 animateBars(analyzerData.analyzer, canvas, canvasCtx, analyzerData.dataArray, analyzerData.bufferLength);
             };
@@ -96,10 +62,11 @@ export default function AudioVizualizer({audioState}){
         }
       }, [analyzerData]);
       
+    // each time track changes: use Audio Web API to create a new context and an analyzer on the contecxt (to generate wave data)
     const audioAnalyzerCallback = useCallback(() => {
         audioCtxRef.current = new (window.AudioContext || window.webkitAudioContext)();
         const audioCtx = audioCtxRef.current;
-        // create an analyzer node with a buffer size of 2048
+        // create an analyzer node with a buffer size of.. 256 or 2048 are common
         analyzerRef.current = audioCtx.createAnalyser();
         const analyzer = analyzerRef.current;
         analyzer.fftSize = 256; // original was 2048
@@ -108,7 +75,6 @@ export default function AudioVizualizer({audioState}){
         const dataArray = new Uint8Array(bufferLength);
         sourceRef.current = audioCtx.createMediaElementSource(audioState);
         var source = sourceRef.current;
-        console.log("Created a source");
         source.connect(analyzer);
         source.connect(audioCtx.destination);
         source.onended = () => {
@@ -127,7 +93,7 @@ export default function AudioVizualizer({audioState}){
 
       useEffect(() => {
         if(sourceRef.current){
-            sourceRef.current.disconnect(); // indien vorige ref niet disconnect zal volume trapgewijs verhogen..
+            sourceRef.current.disconnect(); // unexcpected behavior if previous ref is not disconnected, such as increasing volume each time a new context is made..
           }
           if(audioState){
             audioAnalyzerCallback();
--- ClientApp/src/components/MusicPlayerInReact.js
+++ ClientApp/src/components/MusicPlayerInReact.js
@@ -10,31 +10,11 @@ const MusicPlayerInReact = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [filteredSongs, setFilteredSongs] = useState([]);
   const [isShuffled, setIsShuffled] = useState(false);
-  //const canvasRef = useRef(null);
   const previousSongIndexRef = useRef(null);
   var currentTime = useRef(0);
-
-  //const [analyzerData, setAnalyzerData] = useState(null);
-  //const audioCtxRef = useRef(null);
-  //const analyzerRef = useRef(null);
-  //const sourceRef = useRef(null);
   const [audioState, setAudioState] = useState(audioRef.current);
 
-  /*const fetchMusicList = async () => {
-    try {
-      const response = await fetch('/ytApi/tracknames');
-      if (!response.ok) {
-        throw new Error('Request failed');
-      }
-      const data = await response.json();
-      setMusicFiles(data);
-      setFilteredSongs(data);
-      //handleSearch(data)
-      setCurrentSongIndex(0);
-    } catch (error) {
-      console.error('Error:', error);
-    }
-  };*/
+  // fetch track info from backend
   const fetchMusicAll = async () => {
     try {
       const response = await fetch('/ytApi/tracks');
@@ -42,7 +22,6 @@ const MusicPlayerInReact = () => {
         throw new Error('Request failed');
       }
       const data = await response.json();
-      //console.log(data[0]);
       setMusicFiles(data);
       setFilteredSongs(data);
       setCurrentSongIndex(0);
@@ -51,6 +30,7 @@ const MusicPlayerInReact = () => {
     }
   };
 
+  // handle button events and playback behavior
   const handlePlay = () => {
     setIsPlaying(true);
   };
@@ -80,15 +60,12 @@ const MusicPlayerInReact = () => {
     setCurrentSongIndex(index);
     setIsPlaying(true);
   }
+  // handle search bar behavior
   const handleSearch = (event) => {
     const query = event.target.value;
     setSearchQuery(query);
     setIsPlaying(false);
   
-    /*const filtered = musicFiles.filter((song) =>
-      song.toLowerCase().includes(query.toLowerCase())
-    );*/
-    console.log(musicFiles[0]);
     const filtered = musicFiles.filter((song) =>
       (song.fileName + song.title + song.artist + song.album).toLowerCase().includes(query.toLowerCase())
     );
@@ -97,123 +74,21 @@ const MusicPlayerInReact = () => {
 
   // Fetch music files when the component mounts
   useEffect(() => {
-    //fetchMusicList();
     fetchMusicAll();
   }, []);
-/*
-  const audioAnalyzer = () => {
-    // create a new AudioContext
-    audioCtxRef.current = new (window.AudioContext || window.webkitAudioContext)();
-    const audioCtx = audioCtxRef.current;
-    // create an analyzer node with a buffer size of 2048
-    analyzerRef.current = audioCtx.createAnalyser();
-    const analyzer = analyzerRef.current;
-    analyzer.fftSize = 256; // original was 2048
 
-    const bufferLength = analyzer.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    sourceRef.current = audioCtx.createMediaElementSource(audioRef.current);
-    var source = sourceRef.current;
-    console.log("Created a source");
-    source.connect(analyzer);
-    source.connect(audioCtx.destination);
-    source.onended = () => {
-        source.disconnect();
-        console.log("Disconnected a source");
-    };
-
-    // set the analyzerData state with the analyzer, bufferLength, and dataArray
-    setAnalyzerData({ analyzer, bufferLength, dataArray });
-  };
-
-  function animateBars(analyser, canvas, canvasCtx, dataArray, bufferLength) {
-    // Analyze the audio data using the Web Audio API's `getByteFrequencyData` method.
-    analyser.getByteFrequencyData(dataArray);
-  
-    // Set the canvas fill style to black.
-    canvasCtx.fillStyle = '#000';
-  
-    // Calculate the height of the canvas.
-    //const HEIGHT = canvas.height / 2;
-    const HEIGHT = canvas.height;
-  
-    // Calculate the width of each bar in the waveform based on the canvas width and the buffer length.
-    var barWidth = Math.ceil(canvas.width / bufferLength); // original was * 2.5
-  
-    // Initialize variables for the bar height and x-position.
-    let barHeight;
-    let x = 0;
-  
-    // Loop through each element in the `dataArray`.
-    for (var i = 0; i < bufferLength; i++) {
-      // Calculate the height of the current bar based on the audio data and the canvas height.
-      barHeight = (dataArray[i] / 255) * HEIGHT;
-  
-      // Generate random RGB values for each bar.
-      //const maximum = 10;
-      //const minimum = -10;
-      //var r = 242 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-      //var g = 104 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-      //var b = 65 + Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-      var r = 0; var g = 169; var b = 223;
-  
-      // Set the canvas fill style to the random RGB values.
-      canvasCtx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
-  
-      // Draw the bar on the canvas at the current x-position and with the calculated height and width.
-      //canvasCtx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-      canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-  
-      // Update the x-position for the next bar.
-      x += barWidth + 1;
-    }
-  }
-
-  // Function to draw the waveform
-  const drawCallback = useCallback(() => {
-      const canvas = canvasRef.current;
-      if (!canvas || !analyzerData.analyzer) return;
-      const canvasCtx = canvas.getContext("2d");
-  
-      const animate = () => {
-        requestAnimationFrame(animate);
-        //canvas.width = canvas.width;
-        canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
-        animateBars(analyzerData.analyzer, canvas, canvasCtx, analyzerData.dataArray, analyzerData.bufferLength);
-      };
-  
-      animate();
-  }, [analyzerData]);
-  
-
-  // Effect to draw the waveform on mount and update
-  useEffect(() => {
-    drawCallback();
-  }, [analyzerData, drawCallback]);
-*/
   // Handle playback when the current song index changes
   useEffect(() => {
     if (currentSongIndex !== null) {
       if (isPlaying) {
-        // Play the audio
-        // Assumes musicFiles contain the full URLs to the audio files
-        //const audio = new Audio(`ytApi/audio?fileName=${musicFiles[currentSongIndex]}`);
-        //const audio = document.getElementById('audio-player');
-        //audio.play();
+        // Play the audio, if same song index resume position (pause behavior)
         if(previousSongIndexRef.current === currentSongIndex){
           audioRef.current.currentTime = currentTime.current;
         }
-        //audioRef.current.volume = 1; // workaround volume verhoogt elke song
         audioRef.current.play();
-        setAudioState(audioRef.current);
-        /*if(sourceRef.current){
-          sourceRef.current.disconnect(); // indien vorige ref niet disconnect zal volume trapgewijs verhogen..
-        }
-        audioAnalyzer();*/
+        setAudioState(audioRef.current); // use state for active audio to update visualizer
       } else {
-        // Pause the audio
-        //const audio = document.getElementById('audio-player');
-        //audio.pause();
+        // Pause the audio, save song position
         currentTime.current = audioRef.current.currentTime;
         audioRef.current.pause();
       }
@@ -221,14 +96,13 @@ const MusicPlayerInReact = () => {
     }
   }, [currentSongIndex, isPlaying, filteredSongs]);
 
+  // helper function for settings src on Audio element
   const getAudioSource = () => {
     var song = filteredSongs[currentSongIndex].fileName;
-    //setAudioState(audioRef.current);
-    //console.log(`ytApi/audio?fileName=${song}`);
     return isPlaying? `ytApi/audio?fileName=${song}` : null;
-    //return isPlaying? `ytApi/audio?fileName=${filteredSongs[currentSongIndex].fileName}` : null
   }
 
+  // music library, search bar, list view and media controls fixed at the bottom. Visualizer is seperate component, shown above the audio bar
   return (
     <>
     <p id="lib-title">Music Library</p>
@@ -238,21 +112,10 @@ const MusicPlayerInReact = () => {
           <input id='search-lib' type="text" value={searchQuery} onChange={handleSearch} placeholder="Search songs..."/>
           <div className='scroll-pane'>
             <div className='content-wrapper'>
-          {/*<ol id='audio-library'>
-            {filteredSongs.map((file, index) => (
-              <li key={index} onDoubleClick={() => handleDoubleClick(index)} className={currentSongIndex === index && isPlaying ? 'active' : ''}>{file}</li>
-            ))}
-            </ol>
-            */}
             <table className="table table-striped" aria-labelledby="tableLabel">
               <thead>
                 <tr>
-                  <th>Nr</th>
-                  <th>FileName</th>
-                  <th>Title</th>
-                  <th>Artist</th>
-                  <th>Album</th>
-                  <th>Duration</th>
+                  <th>Nr</th><th>FileName</th><th>Title</th><th>Artist</th><th>Album</th><th>Duration</th>
                 </tr>
               </thead>
               <tbody>
