3153774176591f8431fc164ee4e4a55c4c561b40
thomasv7877 <tvandenhauwe@gmail.com>
2023-06-20 20:23:02 +0200

    edit about page

--- ClientApp/src/components/About.js
+++ ClientApp/src/components/About.js
@@ -4,6 +4,7 @@ export function About(){
 
     return(<div className="col-md-6 offset-md-1" style={{"marginTop": "5%"}}>
     <h3>About</h3>
+    <p>Search youtube, rip audio (using yt-dlp) and playback in a music library view.</p>
     <p id="signature">WireTube made by <span id="firstname">Thomas VdH</span></p>
     </div>)
 }
